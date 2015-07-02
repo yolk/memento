1805bb3d8f251b661408012ae50a5c7111e608e2
Jens Kraemer <jk@jkraemer.net>
2015-07-02 12:33:36 +0200

    make migration generator work in Rails 4
    - added a railtie
    - fixed the generator class
    - also updated migration to use #change instead up/down and prevent deprecation
      warnings for timestamps

---
+++ lib/memento/railtie.rb
@@ -0,0 +1,8 @@
+module Memento
+  class Railtie < Rails::Railtie
+    generators do
+      require File.join File.dirname(__FILE__), '..', '..', 'generators', 'memento_migration', 'memento_migration_generator'
+    end
+  end
+end
+
